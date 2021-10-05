37b13556429cac0d6d63aedcb079d32459771c64
TheAmitBarua <89492966+TheAmitBarua@users.noreply.github.com>
2021-10-04 18:55:48 -0700

    Update source.c

--- source.c
+++ source.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+int main(){
+    float m,t,p,l;
+    printf("Cost of food: ");
+    scanf("%f", &m);
+    printf("Enter tax percent: ");
+    scanf("%f",&t);
+    printf("Enter tip percent: ");
+    scanf("%f", &p);
+    printf("\n******Bill******\n");
+    printf("Cost: $%.2f\n",m);
+    printf("Tax: $%.2f\n",(t/100)*m);
+    printf("Tip: $%.2f\n", (p/100)*m);
+    l = m + (t/100)*m + (p/100)*m;
+    printf("Total: %.2f", l);
+    return 0;
+}
 
